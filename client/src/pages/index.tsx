a2e1a29942e815b3522aecc0b7fa368d301504c9
Jairo Carvalho <jairolaurentinocarvalho@gmail.com>
2020-07-23 15:11:48 -0300

    Add typescript

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+export default function Home({ title = "Boilerplate Next App" }: Props) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
